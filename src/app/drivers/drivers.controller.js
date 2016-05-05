5a8db6fc88cdf4bda3055359cab95376d040b2d1
chicobentojr <francisco.bento.jr@hotmail.com>
2016-05-05 01:43:02 -0300

    Add Filter to Drivers

--- src/app/drivers/drivers.controller.js
+++ src/app/drivers/drivers.controller.js
@@ -8,8 +8,14 @@
   /** @ngInject */
   function DriversController(ergastAPI) {
     var vm = this;
+
     vm.filterName = null;
     vm.driversList = [];
+
+    vm.searchFilter = function (driver) {
+      var keyword = new RegExp(vm.nameFilter, 'i');
+      return !vm.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+    };
 
     ergastAPI.getDrivers().success(function (response) {
         vm.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
