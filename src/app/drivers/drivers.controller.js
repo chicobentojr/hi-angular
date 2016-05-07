490442e375d2e4d6fc4376f9ff93269105c475ce
chicobentojr <francisco.bento.jr@hotmail.com>
2016-05-07 01:03:35 -0300

    Add Filters to Drivers
    - Add Drivers Button at Index Navbar

--- src/app/drivers/drivers.controller.js
+++ src/app/drivers/drivers.controller.js
@@ -9,11 +9,17 @@
   function DriversController(ergastAPI) {
     var vm = this;
 
+    vm.nameFilter = null;
     vm.driversList = [];
 
     ergastAPI.getDrivers().success(function(response){
         vm.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
     });
+
+    vm.searchFilter = function (driver) {
+        var keyword = new RegExp(vm.nameFilter, 'i');
+        return !vm.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+    };
 
   }
 })();
